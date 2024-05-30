51d27e8c878475a8e4611c57cf4a15f0696750c7
Tiago Azevedo <tiagogadenz@icloud.com>
2024-05-30 11:30:03 -0300

    Completing exercises 1.3 to 1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -29,19 +29,29 @@ const Total = (props) => {
 } 
 
 const App = () => {
-  const course = 'Desenvolvimento de aplicação Half Stack'
-  
-  const parts = [
-    { name: 'Fundamentos da biblioteca React', exercises: 10 },
-    { name: 'Usando props para passar dados', exercises: 7 },
-    { name: 'Estado de um componente', exercises: 14 }
-  ]
+  const course = {
+    name: 'Desenvolvimento de aplicação Half Stack',
+    parts: [
+      {
+        name: 'Fundamentos da biblioteca React',
+        exercises: 10
+      },
+      {
+        name: 'Usando props para passar dados',
+        exercises: 7
+      },
+      {
+        name: 'Estado de um componente',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
